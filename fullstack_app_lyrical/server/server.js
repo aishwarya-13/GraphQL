7f0a0a1949db60285cc9dca046b4fb6293416d30
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2023-05-26 20:02:32 -0700

    Removed mongo uri

--- fullstack_app_lyrical/server/server.js
+++ fullstack_app_lyrical/server/server.js
@@ -8,8 +8,7 @@ const schema = require("./schema/schema");
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI =
-  "mongodb+srv://rohitborade:WAsY5MFTaaRZ3xiR@cluster0.9otq6fc.mongodb.net/";
+const MONGO_URI = "";
 if (!MONGO_URI) {
   throw new Error("You must provide a MongoLab URI");
 }
