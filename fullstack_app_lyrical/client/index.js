6cd0e8bec930bba9ef97d2d1332476f77c209ddc
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2023-05-26 19:56:17 -0700

    Setting up Apollo client

--- fullstack_app_lyrical/client/index.js
+++ fullstack_app_lyrical/client/index.js
@@ -1,11 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+/**
+ * 
+Apollo Provider: Integration layer between the Apollo store and our actual React app.
+Provider will take the data from the store and inject it into our React app
+
+Apollo Client: Interacts with GraphQL server on the backend. It is making the request and storing that data locally when the response comes back.
+ */
+
+import React from "react";
+import ReactDOM from "react-dom";
+import ApolloClient from "apollo-client";
+import { ApolloProvider } from "react-apollo";
+
+const client = new ApolloClient({});
 
 const Root = () => {
-  return <div>Lyrical</div>
+  return (
+    <ApolloProvider client={client}>
+      <div>{`Lyrical`}</div>
+    </ApolloProvider>
+  );
 };
 
-ReactDOM.render(
-  <Root />,
-  document.querySelector('#root')
-);
+ReactDOM.render(<Root />, document.querySelector("#root"));
