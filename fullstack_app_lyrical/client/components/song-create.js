aa17b368262f1e9e9a7d5fbd833244a8851fe8c9
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2023-05-31 16:51:25 -0700

    Calling a mutation

--- fullstack_app_lyrical/client/components/song-create.js
+++ fullstack_app_lyrical/client/components/song-create.js
@@ -1,14 +1,58 @@
 import React, { Component } from "react";
+import gql from "graphql-tag";
+import { graphql } from "react-apollo";
+
+const mutation = gql`
+  mutation AddSong($title: String) {
+    addSong(title: $title) {
+      id
+      title
+    }
+  }
+`;
 
 class SongCreate extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.createSong = this.createSong.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      title: e.target.value,
+    });
+  }
+
+  createSong(e) {
+    e.preventDefault();
+    //Call a mutation and sending dynamic data to the mutation
+    this.props.mutate({
+      variables: {
+        title: this.state.title,
+      },
+    });
+    console.log(this.props);
+  }
+
   render() {
     return (
       <div>
         <h3>Create a new song</h3>
-        <form></form>
+        <form onSubmit={this.createSong}>
+          <label>Song Title:</label>
+          <input
+            type={"text"}
+            value={this.state.title}
+            onChange={this.handleChange}
+          />
+        </form>
       </div>
     );
   }
 }
 
-export default SongCreate;
+export default graphql(mutation)(SongCreate);
